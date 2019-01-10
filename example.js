d0344e3799725d27b5a77842b8a872153fe8ebdd
YPW <shiwanfan@163.com>
2019-01-10 12:13:57 +0800

    update jquery

---
+++ example.js
@@ -0,0 +1,4 @@
+var s = document.getElementById('purchases');
+var i = s.getElementsByTagName('*');
+alert(i.length);
+alert(i[0]);
---
+++ learnOOP.js
@@ -0,0 +1,5 @@
+let screenHeight = window.innerHeight || document.body.clientHeight;
+let screenWidth = window.innerWidth || document.body.clientWidth;
+console.log('height: '+ screenHeight + ' ' + 'weight: ' + screenWidth);
+console.log('navigator: ', navigator);
+console.log('location: ', location);
